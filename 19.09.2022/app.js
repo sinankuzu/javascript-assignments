82e58d542a83863692f6caef8f9d97898cd8fa03
Sinan <sinankzu@gmail.com>
2022-09-19 17:03:40 +0200

    art

--- 19.09.2022/app.js
+++ 19.09.2022/app.js
@@ -1,4 +1,8 @@
+//BASLIK KISMI ORTALAMA (dogukandan. boyle yapmayi unutmusum hatirlanmali.)
 
+// const baslik = document.querySelector("h1");
+// baslik.style.textAlign = "center";
+// baslik.style.color = "red";
 
 const degisenResimler = document.querySelector(".imgs");
 const araButonu = document.querySelector(".ara");
@@ -7,24 +11,24 @@ const konusButonu = document.querySelector(".konus");
 const calmaSesi = document.querySelector("#calma-sesi"); // yada new Audio("kaynak")
 // const calmaSesi = new Audio("./img/retro-ringtone .mp3")
 
-degisenResimler.onmouseover = ()=>{
-    degisenResimler.src = "./img/aslan1.jpeg"
+degisenResimler.onmouseover = () => {
+  degisenResimler.src = "./img/aslan1.jpeg";
 };
 
-degisenResimler.onmouseout = ()=>{
-    degisenResimler.src = "./img/aslan2.jpeg"
+degisenResimler.onmouseout = () => {
+  degisenResimler.src = "./img/aslan2.jpeg";
 };
 
-araButonu.onclick = ()=>{
-    degisenResimler.src ="./img/calan-telefon.gif"
-    calmaSesi.play();
+araButonu.onclick = () => {
+  degisenResimler.src = "./img/calan-telefon.gif";
+  calmaSesi.play();
 };
 
-baglatButonu.onclick = ()=>{
-    degisenResimler.src = "./img/telbağla.gif"
-    calmaSesi.pause();
-}
+baglatButonu.onclick = () => {
+  degisenResimler.src = "./img/telbağla.gif";
+  calmaSesi.pause();
+};
 
-konusButonu.onclick = () =>{
-    degisenResimler.src = "./img/telefon.gif"
-}
+konusButonu.onclick = () => {
+  degisenResimler.src = "./img/telefon.gif";
+};
