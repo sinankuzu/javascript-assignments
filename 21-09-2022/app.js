a95a9f51ea13b18f243c4232ef592fddf4d7698b
Sinan <sinankzu@gmail.com>
2022-09-21 15:03:14 +0200

    go

--- 21-09-2022/app.js
+++ 21-09-2022/app.js
@@ -2,11 +2,16 @@ const myTable = document.querySelector(".table1");
 const myButton = document.querySelector(".hesapla");
 const krediMiktari = document.querySelector(".kredi-miktari").value;
 const krediTipi = document.querySelector(".kredi-tipi").value;
+const myVade = document.querySelector(".vade").value;
+
 
 myButton.onclick = () =>{
     const krediMiktari = document.querySelector(".kredi-miktari").value;
     const krediTipi = document.querySelector(".kredi-tipi").value;
-myTable.innerHTML = `<table class="table table-bordered border-warning  mt-4">
+    const myVade = document.querySelector(".vade").value;
+    const taksitTutari =
+      krediMiktari *
+      (myTable.innerHTML = `<table class="table table-bordered border-warning  mt-4">
   <tbody>
     <tr>
       <th>Kredi Miktari</th>
@@ -14,20 +19,13 @@ myTable.innerHTML = `<table class="table table-bordered border-warning  mt-4">
       <th>Kredi Tipi</th>
       <td>${krediTipi}</td>
       </tr>
-      <tr>
-       <th>Vade</th>
-      <td>${krediMiktari} ₺</td>
-      <th>Faiz Orani</th>
-      <td>${krediTipi}</td>
-       </tr>
        <tr>
-       <th>Toplam Tutar</th>
-      <td>${krediMiktari} ₺</td>
-      <th>Taksit Tutari</th>
-      <td>${krediTipi}</td>
-       </tr>
+        <th>Vade</th>
+       <td>${myVade} ₺</td>
+    </tr>
+   
     ..............
   </tbody>
-</table>;`;
+</table>;`);
 }
 
