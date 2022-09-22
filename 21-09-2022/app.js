8dc78d65fed9418c77b81f5977aada99337cb073
Sinan <sinankzu@gmail.com>
2022-09-22 20:24:22 +0200

    gola

--- 21-09-2022/app.js
+++ 21-09-2022/app.js
@@ -3,12 +3,24 @@ const myButton = document.querySelector(".hesapla");
 const krediMiktari = document.querySelector(".kredi-miktari").value;
 const krediTipi = document.querySelector(".kredi-tipi").value;
 const myVade = document.querySelector(".vade").value;
-
+let faizOrani = 0;
 
 myButton.onclick = () =>{
     const krediMiktari = document.querySelector(".kredi-miktari").value;
     const krediTipi = document.querySelector(".kredi-tipi").value;
     const myVade = document.querySelector(".vade").value;
+
+    if(select.value == "Konut Kredisi"){
+      faizOrani = 1.29;
+    }
+
+    else if(select.value == "Ihtiyac Kredisi"){
+      faizOrani = 1.99;
+    }
+
+    else if(select.value == "Arac Kredisi"){
+      1.79
+    }
     const taksitTutari =
       krediMiktari *
       (myTable.innerHTML = `<table class="table table-bordered border-warning  mt-4">
@@ -22,6 +34,14 @@ myButton.onclick = () =>{
        <tr>
         <th>Vade</th>
        <td>${myVade} ₺</td>
+       <th>Kredi Tipi</th>
+      <td>${krediTipi}</td>
+    </tr>
+    <tr>
+        <th>Vade</th>
+       <td>${myVade} ₺</td>
+       <th>Kredi Tipi</th>
+      <td>${krediTipi}</td>
     </tr>
    
     
