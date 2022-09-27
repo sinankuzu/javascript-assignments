f1f9e812597633dc3197d2c1dae4ab9da4840e4e
Sinan <sinankzu@gmail.com>
2022-09-27 10:43:34 +0200

    prettier

--- to-do-app/app.js
+++ to-do-app/app.js
@@ -6,38 +6,29 @@ const myIcon = document.createElement("fa-check");
 const myToplam = document.querySelector("#toplam");
 const myTamamlanan = document.querySelector("#tamamlanan");
 
+todoButton.onclick = () => {
+  todoLi.innerHTML += `<li><i class="fa-solid fa-check"></i> <p>${todoInput.value}</p><i class="fa-solid fa-trash"></i></li>`;
+  myToplam.innerHTML++;
+  todoInput.value = "";
+};
 
-
-todoButton.onclick= () => {
- todoLi.innerHTML += `<li><i class="fa-solid fa-check"></i> <p>${todoInput.value}</p><i class="fa-solid fa-trash"></i></li>`;
- myToplam.innerHTML ++;  
- todoInput.value = "";
- }
-
- todoLi.onclick= (e)=> {
-
-    if (e.target.classList.contains("fa-trash")) {
-      e.target.parentElement.remove();
-      myToplam.innerHTML--;
-      if (myTamamlanan.value == myToplam.value && e.target.parentElement.classList.contains("checked")){
-        myTamamlanan.innerHTML--;
-      }
-    } 
-    else if (e.target.parentElement.classList.contains("checked")) {
-      e.target.parentElement.classList.remove("checked");
+todoLi.onclick = (e) => {
+  if (e.target.classList.contains("fa-trash")) {
+    e.target.parentElement.remove();
+    myToplam.innerHTML--;
+    if (
+      myTamamlanan.value == myToplam.value &&
+      e.target.parentElement.classList.contains("checked")
+    ) {
       myTamamlanan.innerHTML--;
-    } 
-    else if (e.target.classList.contains("fa-check")) {
-      e.target.parentElement.classList.add("checked");
-      myTamamlanan.innerHTML++;
-    } 
-    else {
-      alert("buraya basmayin");
     }
-    
-    
- }
- 
-
-
-
+  } else if (e.target.parentElement.classList.contains("checked")) {
+    e.target.parentElement.classList.remove("checked");
+    myTamamlanan.innerHTML--;
+  } else if (e.target.classList.contains("fa-check")) {
+    e.target.parentElement.classList.add("checked");
+    myTamamlanan.innerHTML++;
+  } else {
+    alert("buraya basmayin");
+  }
+};
