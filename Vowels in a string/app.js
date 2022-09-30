420b580cc5f6418dfafe01f35bc22616be47a05f
Sinan <sinankzu@gmail.com>
2022-09-30 16:51:53 +0200

    bitti ekler kelnedi

--- Vowels in a string/app.js
+++ Vowels in a string/app.js
@@ -1,19 +1,29 @@
 const myInput = document.querySelector(".input1");
 const myButton = document.querySelector(".mybutton");
+const myResult = document.querySelector(".result");
 let myArray =[]
 const vowels = ["a","e","i","u","o"];
 
 
 
 myButton.onclick = () => {
+
 let sayac = 0;
- myArray = myInput.value.split("");
+let myInput2 = myInput.value.toLowerCase();
+ myArray = myInput2.split("");
  myArray.map((x) =>{
    if(vowels.includes(x)){
       sayac += 1;
    }
  })
- console.log(`There are ${sayac} vowels have found in your sentence`);
+ if(sayac > 1){
+ myResult.textContent = `There are ${sayac} vowels have found in your sentence`;
+
+ }
+ else{
+   myResult.textContent = `Your sentence does not contain a vowel`;
+ }
+ 
 }
 
 
