746051c6f51ffbee319c51bde0871e692f52818c
Sinan <sinankzu@gmail.com>
2022-10-01 19:15:46 +0200

    bitit heralde

--- FETCH-API/script.js
+++ FETCH-API/script.js
@@ -1,7 +1,8 @@
 
 
-const myData = fetch("./tv-shows.json").then((res) => res.json()).then((data) => ekranagetir(data));
+const myData = fetch("./tv-shows.json").then((res) => res.json()).then((data) =>ekranagetir(data));
 const myButton = document.querySelector(".buton");
+
 
 const ekranagetir = (osman) => {
     const myCards = document.querySelector(".cards");
@@ -11,10 +12,10 @@ const ekranagetir = (osman) => {
        
           myCards.innerHTML += `<div class="container border">
           <div class="card text-center" style="width: 18rem;">
-  <img class="card-img-top" src="${ahmet.show.image.medium}" alt="Card image cap">
+//   <img class="card-img-top" src="${ahmet.show.image.medium}" alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">${ahmet.show.name}</h5>
-    <a href="#" class="btn btn-primary buton">Detaylar</a>
+    <a href="${ahmet.show.url}" class="btn btn-primary buton">Detaylar</a>
   </div>
 </div>
 </div>`;
@@ -22,15 +23,4 @@ const ekranagetir = (osman) => {
 
 }
 
-myButton.onclick = () => {
-  myCards.innerHTML += `<div class="container border">
-          <div class="card text-center" style="width: 18rem;">
-  <img class="card-img-top" src="${ahmet.show.image.medium}" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">${ahmet.show.name}</h5>
-    <a href="${ahmet.show.url}" class="btn btn-primary">Detaylar</a>
-    <a href="#" class="btn btn-primary">Detaylar</a>
-  </div>
-</div>
-</div>`;
-};
+
