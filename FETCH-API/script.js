fe506893b5d6a0f60b771cb50055062d4e71f676
Sinan <sinankzu@gmail.com>
2022-10-04 12:32:51 +0200

    guncel

--- FETCH-API/script.js
+++ FETCH-API/script.js
@@ -7,12 +7,12 @@ const myButton = document.querySelector(".buton");
 const ekranagetir = (osman) => {
     const myCards = document.querySelector(".cards1");
     
-    osman.forEach(ahmet => {
+    osman.forEach((ahmet) => {
        
        
           myCards.innerHTML += `
           <div class="card text-center col-4 m-2" style="width: 18rem;">
-   <img class="card-img-top" src="${ahmet.show.image.medium}" alt="Card image cap">
+   <img class="card-img-top" src="${ahmet.show.image?ahmet.show.image.medium:""}" alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">${ahmet.show.name}</h5>
     <a href="${ahmet.show.url}" class="btn btn-primary buton">Detaylar</a>
